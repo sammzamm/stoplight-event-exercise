e686b0f1b300a0fdaec11aa18a2b42306078bc16
sammzamm <sammzamm01@gmail.com>
2017-02-20 17:21:26 -0700

    stoplight final product

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 (function() {
-  'use strict';
+    'use strict';
+    var controls = document.getElementById('controls');
 
-  // YOUR CODE HERE
+    var go = document.getElementById('goButton');
+    var green = document.getElementById('goLight')
+
+    var stop = document.getElementById('stopButton');
+    var red = document.getElementById('stopLight');
+
+    var slow = document.getElementById('slowButton');
+    var orange = document.getElementById('slowLight');
+
+    controls.addEventListener('click', function() {
+        if (event.target.id === "stopButton") {
+            red.classList.toggle('stop');
+        }
+    })
+    controls.addEventListener('click', function() {
+        if (event.target.id === "slowButton") {
+            orange.classList.toggle('slow');
+        }
+    })
+    controls.addEventListener('click', function() {
+        if (event.target.id === "goButton") {
+            green.classList.toggle('go');
+        }
+    })
 })();
+
+controls.addEventListener('mouseover', function() {
+    if (event.target.id === 'stopButton' || event.target.id === 'goButton' || event.target.id === 'slowButton') {
+        console.log('Mouse hovered over the ' + event.target.innerText + ' button');
+    }
+})
+
+controls.addEventListener('mouseout', function() {
+    if (event.target.id === 'stopButton' || event.target.id === 'goButton' || event.target.id === 'slowButton') {
+        console.log('Mouse left the ' + event.target.innerText + ' button');
+    }
+})
+
+// controls.addEventListener('mouseoff', function(){})
